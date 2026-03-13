05e552ab5dd4158cf04b82f5774a292e2271b746
Nozhin Azarpanah <azarpann@mcmaster.ca>
2026-03-13 00:36:38 -0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,26 @@
+window.addEventListener('load', function() {
+    const scrollTopBtn = document.getElementById('scrollTopBtn');
+    const hamburger = document.getElementById('hamburger');
+    const navLeft = document.querySelector('.nav-left');
+    const navRight = document.querySelector('.nav-right');
+
+    window.addEventListener('scroll', () => {
+        if (window.scrollY > 10) {
+            scrollTopBtn.classList.add('show');
+        } else {
+            scrollTopBtn.classList.remove('show');
+        }
+    });
+
+    scrollTopBtn.addEventListener('click', () => {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    });
+
+    hamburger.addEventListener('click', () => {
+        navLeft.classList.toggle('active');
+        navRight.classList.toggle('active');
+
+        const expanded = hamburger.getAttribute('aria-expanded') === 'true' || false;
+        hamburger.setAttribute('aria-expanded', !expanded);
+    });
+});
