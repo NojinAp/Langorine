63b12d65eb054f2e791ae0475b01329c3e19fb00
Nozhin Azarpanah <azarpann@mcmaster.ca>
2026-03-14 00:20:34 -0400

    Added new section

--- js/script.js
+++ js/script.js
@@ -1,8 +1,24 @@
-window.addEventListener('load', function() {
+window.addEventListener('DOMContentLoaded', () => {
+
     const scrollTopBtn = document.getElementById('scrollTopBtn');
     const hamburger = document.getElementById('hamburger');
     const navLeft = document.querySelector('.nav-left');
     const navRight = document.querySelector('.nav-right');
+    const elements = document.querySelectorAll(".reveal");
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+
+            if (entry.isIntersecting) {
+                entry.target.classList.add("show");
+            }
+
+        });
+    }, {
+        threshold: 0.15
+    });
+
+    elements.forEach(el => observer.observe(el));
 
     window.addEventListener('scroll', () => {
         if (window.scrollY > 50) {
@@ -23,4 +39,5 @@ window.addEventListener('load', function() {
         const expanded = hamburger.getAttribute('aria-expanded') === 'true' || false;
         hamburger.setAttribute('aria-expanded', !expanded);
     });
+
 });
